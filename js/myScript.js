c95e623d703ffd7184269068c0cf1e5a9c4ec931
Rik Bakker <ra.bakkeralfacollege@gmail.com>
2022-12-14 09:57:26 +0100

    first commit - committing all files to the repository

---
+++ js/myScript.js
@@ -0,0 +1,9 @@
+function myFunction() {
+  document.getElementById("prank").innerHTML="<br><br>Niet daar :)";
+}  
+
+function Disclaimer() {
+  onclick="document.getElementById('dis').style.display='block'";
+}
+
+
