9b6772a20f37d70a955f5130fab397dfdfca2ef8
rikarjenbakker <ra.bakkeralfacollege@gmail.com>
2023-01-09 13:32:03 +0100

    nieuwe pagina aangemaakt en nav aangepast

--- js/myScript.js
+++ js/myScript.js
@@ -6,4 +6,7 @@ function Disclaimer() {
   onclick="document.getElementById('dis').style.display='block'";
 }
 
-
+function persoonlijkheid() {
+  document.getElementById("source").style.display='block';
+  document.getElementById("button").style.display='none';
+}
